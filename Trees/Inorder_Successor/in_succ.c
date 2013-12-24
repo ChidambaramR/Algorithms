9cf40b09410feaae4f46ac5c9c7d4643348ffb64
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2013-12-24 09:23:57 -0500

    Sorted array/LL to BST

--- Trees/Inorder_Successor/in_succ.c
+++ Trees/Inorder_Successor/in_succ.c
@@ -19,7 +19,7 @@ void insert(struct node **root, int data){
 		return;
 	}
 
-	if(data < (*root)->left)
+	if(data < (*root)->data)
 		insert(&((*root)->left), data);
 	else
 		insert(&((*root)->right), data);
@@ -54,7 +54,12 @@ struct node* inorder_succ(struct node *root, int target){
 	if(temp == NULL)
 		return 0;
 
-	// Min value in the right sub tree
+	/*
+	If right sub tree is not NULL, then the left most child
+	in the right sub-tree is the in-order successor.
+	If the right child does not have a left child, then the 
+	right child is the in-order successor.
+	*/
 	if(temp->right){
 		current = temp->right;
 		while(current->left)
@@ -62,10 +67,40 @@ struct node* inorder_succ(struct node *root, int target){
 		return current;
 	}
 
+	/*
+	1. If right sub-tree is NULL, then get the parent pointer
+	of the current node, traverse up the tree, till the node we
+	are visiting is the left child of its parent. If one such node is found
+	its parent is the in-order successor.
+	*/
 	current = temp->parent;
-	while((current != NULL)
+	while((current != NULL) && root == current->right){
+		root = current;
+		current = current->parent;
+	}
+	
+	return current;
 }
 
+
+
 int main(){
+	int n, no;
+	struct node *root, *in_suc;
+	root = (struct node*)malloc(sizeof(struct node));
+	root->left = root->right = NULL;
+	root->data = 20;
+
+	printf("Enter the size of input: ");
+	scanf("%d",&n);
+	while(n--){
+		printf("Enter the number ");
+		scanf("%d",&no);
+		insert(&root, no);
+	}
+	printf("Enter the no for which you want to find inorder_succ ");
+	scanf("%d",&n);
+	in_suc = inorder_succ(root, n);
+	printf("In order successor of %d is %d\n",n,in_suc->data);
 	return 0;
 }
