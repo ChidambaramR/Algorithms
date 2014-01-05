4f8c880f019dd0cf6ba9de65f2938854fe23dc2a
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2014-01-05 00:04:54 -0500

    String questions

--- String_Ops/Remove_duplicate_in_string/rem_duplicate.c
+++ String_Ops/Remove_duplicate_in_string/rem_duplicate.c
@@ -7,7 +7,7 @@ bool hash[256];
 void rem_dup(char *str){
 	//int len = strlen(str);
 	int ip_ind=1, res_ind=1;
-	while(*(str+ip_ind)){
+	while(str[ip_ind]){
 		hash[str[ip_ind] - 'a']++;
 		if(hash[str[ip_ind] - 'a'] == 1){
 			str[res_ind] = str[ip_ind];
@@ -21,12 +21,31 @@ void rem_dup(char *str){
 	}
 }
 
+
+void rem_dup_efficient(char *str){
+	int length = strlen(str);
+	int tail = 1;
+	int i,j;
+
+	for(i=1; i<length; i++){
+		for(j=0; j<tail; j++){
+			if(str[j] == str[i])
+				break;
+		}
+		if(j == tail){
+			str[tail] = str[i];
+			tail++;
+		}
+	}
+	str[tail] = '\0';
+}
+
 int main(){
 	char a[100];
 	printf("Enter the string to rem duplicates ");
 	scanf("%s",a);
 	hash[a[0] - 'a']++;
-	rem_dup(a);
+	rem_dup_efficient(a);
 	printf("\nThe string after removing duplicates: %s\n",a);
 	return 0;
 }
