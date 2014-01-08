b618ddb2eab927736303cbbc45ab76d2ce6181bd
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2014-01-08 18:57:30 -0500

    Adding digits in a LL

--- Linked_List/Add_digits_LL/add_digits.c
+++ Linked_List/Add_digits_LL/add_digits.c
@@ -8,14 +8,19 @@ struct node{
 
 struct node *head_c;
 
-void push(struct node *head, int data){
+void push(struct node **head, int data){
   struct node *temp, *crawl;
-  crawl = head;
+  crawl = *head;
+
   // Create a temporary node
   temp = (struct node*)malloc(sizeof(struct node));
   temp->next = NULL;
   temp->data = data;
 
+  if(!(*head)){
+    *head = temp;
+    return;
+  }
   // Loop till the last node
   while(crawl->next)
     crawl = crawl->next;
@@ -25,7 +30,7 @@ void push(struct node *head, int data){
 }
 
 int recurse_sum(struct node *head_a, struct node *head_b){
-	if( head_a == NULL || head_b == NULL)
+	if(head_a == NULL && head_b == NULL)
 		return 0;
 
 	int carry =  0;
@@ -33,27 +38,61 @@ int recurse_sum(struct node *head_a, struct node *head_b){
 
 	carry = recurse_sum(head_a->next, head_b->next);
 	num = (head_a->data + head_b->data + carry);
-	if(num >= 10){
-		struct node *temp = (struct node*)malloc(sizeof(struct node));
-		temp->data = num-10;
-		temp->next = head_c->next;
-		head_c->next = temp;
+	
+	// Create the new node in the LL
+	struct node *temp = (struct node*)malloc(sizeof(struct node));
+	temp->next = head_c;
+	head_c = temp;
+	temp->data = num%10;
+	
+	if(num >= 10)
 		return 1;
-	}
-	else{
-		struct node *temp = (struct node*)malloc(sizeof(struct node));
-		temp->data = num;
-		temp->next = head_c->next;
-		head_c->next = temp;
+	else
 		return 0;
-	}	
 		
 		 
 }
 
 void add_digits(struct node *head_a, struct node *head_b, int len_a, int len_b){
-	if(len_a == len_b)
-		recurse_sum(head_a, head_b);
+	int carry = 0;
+	/*
+	We need this temporary variables because, we need to assign the head pointer
+	of the new list as one of these head pointers. Because, we are just gonna
+	append the bigger list. i.e If the list is 1->2->3->4 and other list is
+	1->2,
+	We calculate the difference in lengths and move the pointer in the bigger list
+	to the same place where the second list is starting. Once the equal length list
+	addition is made, we add the carry to left part of the bigger list and APPEND our
+	new list to this list. Thus the list becomes 1->2->4->6. The 4->6 is the new list
+	and it has been appended to our bigger list 1->2->3->4. Thus if we move the head
+	pointer which we got, then we will loose track of the head pointers of the list
+	to make the append operation and make the new head point to one of the heads which
+	we received. 
+	*/
+	struct node *thead_a, *thead_b;
+	thead_a = head_a;
+	thead_b = head_b;
+	if(len_a > len_b){
+		while(len_a-1 > len_b){
+			len_a--;
+			thead_a = thead_a->next;
+		}
+		carry = recurse_sum(thead_a->next, thead_b);
+		thead_a->data = carry + thead_a->data;
+		thead_a->next = head_c;
+		head_c = head_a;
+		
+	}
+	if(len_b > len_a){
+		while(len_b-1 > len_a){
+			len_b--;
+			thead_b = thead_b->next;
+		}
+		carry = recurse_sum(thead_a, thead_b->next);
+		thead_b->data = carry + thead_b->data;
+		thead_b->next = head_c;
+		head_c = head_b;
+	}
 }
 
 void print(struct node *root){
@@ -71,17 +110,7 @@ int main(){
   int i=1;
   int len_a, len_b;
   struct node *head_a, *head_b;
-  // Create node 1
-  head_a = (struct node*)malloc(sizeof(struct node));
-  head_a->next = NULL;
-
-  // Create node 2
-  head_b = (struct node*)malloc(sizeof(struct node));
-  head_b->next = NULL;
-
-  // Resultant Node
-  head_c = (struct node*)malloc(sizeof(struct node));
-  head_c->next = NULL;
+  head_a = head_b = NULL;
 
   printf("Enter the number of digits in No1: ");
   scanf("%d",&n);
@@ -89,7 +118,7 @@ int main(){
   while(n--){
   printf("Enter digit %d: ",i++);
   scanf("%d",&data);
-  push(head_a, data);
+  push(&head_a, data);
   }
 
   printf("Enter the number of digits in No2: ");
@@ -98,11 +127,11 @@ int main(){
   while(n--){
   printf("Enter digit %d: ",i++);
   scanf("%d",&data);
-  push(head_b, data);
+  push(&head_b, data);
   }
 
-  add_digits(head_a->next, head_b->next, len_a, len_b);
+  add_digits(head_a, head_b, len_a, len_b);
   
-  print(head_c->next);
+  print(head_c);
   return 0;
 }
